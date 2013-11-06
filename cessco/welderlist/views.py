1a8a78fc836b3c0315297d61ad9c346b4bb74793
Robb Sombach <robb@sombach.com>
2013-11-06 15:44:00 -0700

    add date picker and cleanup templates

--- cessco/welderlist/views.py
+++ cessco/welderlist/views.py
@@ -48,8 +48,11 @@ class WelderDetailView(LoginRequiredMixin, DetailView):
         
         # Add in a QuerySet of all the performance qualifications for the current welder
         kwargs_welder_id = kwargs["object"].id
+        
         context['performance_qualification_list'] = PerformanceQualification.objects.filter(welder_id=kwargs_welder_id, active=True)
         self.request.session['current_welder'] = kwargs_welder_id
+        self.request.session['current_welder_first_name'] = kwargs["object"].first_name
+        self.request.session['current_welder_last_name'] = kwargs["object"].last_name
 
         return context
 
@@ -73,29 +76,14 @@ class PerformanceQualificationCreateView(LoginRequiredMixin, WelderListActionMix
         # Call the base implementation first to get a context
         context = super(PerformanceQualificationCreateView, self).get_context_data(**kwargs)
         context['current_welder'] = self.request.session['current_welder']
+        context['current_welder_first_name'] = self.request.session['current_welder_first_name']
+        context['current_welder_last_name'] = self.request.session['current_welder_last_name']
+        
         return context
         
     def form_valid(self, form):
         form.instance.welder_id = self.request.session['current_welder']
         return super(PerformanceQualificationCreateView, self).form_valid(form)
-
-#     def post(self, request, *args, **kwargs):
-# 
-#         pq_form = PerformanceQualificationCreateForm(request.POST)
-#         if pq_form.is_valid():
-#             pq = pq_form.save(commit=False)
-#             pq.welder_id = self.request.session['current_welder']
-#             pq.save()
-#             print "performance_qualification_id: %d" % pq.id
-# #             return super(PerformanceQualificationCreateView, self).form_valid(pq)
-#             return self.form_valid(pq)
-#         else:
-# #             return super(PerformanceQualificationCreateView, self).form_invalid(pq)
-#             return self.form_invalid(pq)
-            
-#     def form_valid(self, form):
-#         form.save()
-#         return super(PerformanceQualificationCreateView, self).form_valid(form)
 
 
 class PerformanceQualificationDetailView(LoginRequiredMixin, DetailView):
